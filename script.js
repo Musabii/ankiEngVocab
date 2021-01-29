fe96a52f7ed9be94eef876311ab8443d54ba562d
Musab I <idkmsb@gmail.com>
2021-01-30 00:24:57 +0300

    started using jquery and started implementing the api

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-const makeButton = document.getElementById('makeButton');
-let wordsInput = document.getElementById('wordsInput');
+const makeButton = $('#makeButton');
+let wordsInput = $('#wordsInput');
 
-function clicked() {
-    let words = wordsInput.value.split(",");
+makeButton.click(function() {
+    let words = wordsInput.val().split("\n");
+    let notes = makeNotes(words)
+    download(notes.join("\n"), "words", ".txt")
+    })
+
+function makeNotes(words) {
+    let notes = []
+    words.forEach( (e, i) => {
+        getData(e)
+        notes[i] = idk
+    })
+    definitions = notes
+    examples = definitions
+    tags = examples
 }
 
-makeButton.addEventListener('click', clicked);
+function getData(word) {
+    let link = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    var defs, exes, tags;
+    data = $.getJSON(link, data => {
+        defs = data[0].meanings[0].definitions;
+        exes = data[0].meanings[0].definitions;
+        tags = data[0].meanings[0].partOfSpeech;
+    })
+    text = word + "\t" + defs + "\t" + exes + "\t" + tags;
+    console.log(data)
+}
 
-
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
