bb8ae502a69f0ebf2bfa57681c04b3f25796f616
Musab I <idkmsb@gmail.com>
2021-02-04 00:19:08 +0300

    i mean it kinda works

--- script.js
+++ script.js
@@ -4,36 +4,43 @@ let wordsInput = $('#wordsInput');
 makeButton.click(function() {
     let words = wordsInput.val().split("\n");
     let notes = makeNotes(words);
+    let failed = listFails(words, notes)
+    console.log(notes);
     //download(notes.join("\n"), "words", ".txt")
+    });
+
+function listFails(words, notes) {
+    let fails = [];
+    notes.forEach( (e, i) => {
+        
     })
+}
 
 function makeNotes(words) {
     let notes = []
-    words.forEach( (e, i) => {
-        console.log(getData(e))
+    words.forEach( (word, i) => {
+        let link = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+        let defs = "", exes = "", tags = "";
+
+        $.getJSON(link, data => {    
+            console.log(data[0].meanings.length)
+            if (data[0].meanings.length == 0) notes[i] = undefined;
+            data[0].meanings.forEach( e => {
+            e.definitions.forEach( (e, i) => {
+                defs += i+1 + "." + " " + e.definition + " ";
+                exes += i+1 + "." + " " + e.example + " ";
+            })
+            tags += e.partOfSpeech + " ";
+        })
+        notes[i] = null;
+        notes[i] = word + "\t" + defs + "\t" + exes + "\t" + tags;
     })
-    definitions = notes
-    examples = definitions
-    tags = examples
+    })
+    return notes;
 }
 
 function getData(word) {
-    let link = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-    defs = "", exes = "", tags = "", note = "";
-
-    $.getJSON(link, data => {    
-        console.log(data[0].meanings.length)
-        if (data[0].meanings.length == 0) return null
-        data[0].meanings.forEach( e => {    
-            e.definitions.forEach( (e, i) => {
-                defs += i+1 + "." + " " + e.definition + "\n\n";
-                exes += i+1 + "." + " " + e.example + "\n\n";
-            })
-            tags += e.partOfSpeech + " ";
-        });
-        note = defs + "\t" + exes + "\t" + tags;
-        return note;
-    })
+    
 }
 
 function download(data, filename, type) {
