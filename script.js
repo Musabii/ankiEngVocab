380105cd19e4a16f1b343bff47a68b5a89e094a7
Musab I <idkmsb@gmail.com>
2021-02-05 20:33:05 +0300

    proto

--- script.js
+++ script.js
@@ -3,44 +3,42 @@ let wordsInput = $('#wordsInput');
 
 makeButton.click(function() {
     let words = wordsInput.val().split("\n");
-    let notes = makeNotes(words);
-    let failed = listFails(words, notes)
-    console.log(notes);
-    //download(notes.join("\n"), "words", ".txt")
-    });
+    makeNotes(words, downloadStuff);
+})
 
-function listFails(words, notes) {
-    let fails = [];
-    notes.forEach( (e, i) => {
-        
-    })
-}
-
-function makeNotes(words) {
-    let notes = []
+function makeNotes(words, callback) {
+    let notes = [], fails = [];
     words.forEach( (word, i) => {
         let link = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
         let defs = "", exes = "", tags = "";
 
-        $.getJSON(link, data => {    
-            console.log(data[0].meanings.length)
-            if (data[0].meanings.length == 0) notes[i] = undefined;
+        $.getJSON(link, data => {
+            if (data[0].meanings.length == 0) return fails.push(word);
             data[0].meanings.forEach( e => {
             e.definitions.forEach( (e, i) => {
                 defs += i+1 + "." + " " + e.definition + " ";
                 exes += i+1 + "." + " " + e.example + " ";
             })
             tags += e.partOfSpeech + " ";
+            })
+        
+            notes.push(word + "\t" + defs + "\t" + exes + "\t" + tags);
         })
-        notes[i] = null;
-        notes[i] = word + "\t" + defs + "\t" + exes + "\t" + tags;
+        .fail(() => {
+            fails.push(word);
+            if (words.length == i+1) callback(notes, fails);
+        })
+        .done(() => {
+            if (words.length == i+1) callback(notes, fails);
+        })
     })
-    })
-    return notes;
+    
 }
 
-function getData(word) {
-    
+let downloadStuff = function(notes, fails) {
+    console.log(notes, fails);
+    download(notes.join("\n"), "ankiNotes", ".txt");
+    download(fails.join("\n"), "failedWords", ".txt");
 }
 
 function download(data, filename, type) {
